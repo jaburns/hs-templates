6b19dd67b31f39c52bcf8ebf6db42dadc9f0cfe1
Jeremy Burns <jeremyaburns@gmail.com>
2013-05-21 05:36:44 +0000

    Added thumbs for my projects

--- Compile.hs
+++ Compile.hs
@@ -12,13 +12,32 @@ import System.Directory (createDirectoryIfMissing)
 
 -------------------------------------------------------------------------------
 
+
+things = map (\x -> [("name",x)]) [
+    "elastoball",
+    "skistuntstudio",
+    "nescorder",
+    "avrcaster",
+    "vgadac",
+    "key360",
+    "tim2adder",
+    "flashtrees",
+    "flashbees",
+    "column",
+    "marble",
+    "ships",
+    "rope",
+    "sphere" ];
+
+
 main = do
     createDirectoryIfMissing True "compiled"
     doc <- loadView "index.view.html"
     panel <- loadView "indexpanel.partial.html"
-    let docWithPanel = insertValues [("name","platformer")]
-                     $ insertContent "panels" (docContent panel) doc
-    writeFile "compiled/index.html" $ renderView $ docWithPanel
+    let panelSet = map (\x -> insertValues x panel) things
+    let panels = concat $ map docContent panelSet
+    let docWithPanels = insertContent "panels" panels doc
+    writeFile "compiled/index.html" $ renderView $ docWithPanels
     
     
     
